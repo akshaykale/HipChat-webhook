904dde7d8fe5666d1a31724995c49cee7f18b3af
akshaykale <ark.akshaykale@gmail.com>
2017-05-16 19:19:13 +0900

    back to post again

--- src/translate.js
+++ src/translate.js
@@ -11,7 +11,7 @@ exports.translate = function(message){
   var URL = "https://rakuten.hipchat.com/v2/room/3414317/notification?auth_token="+process.env.AUTH;
   var translateURL = "http://www.transltr.org/api/translate?text="+encodeURI(msg)+"&to=en";
 
-  restClient.get(translateURL, function (data_tr, res) {
+/*  restClient.get(translateURL, function (data_tr, res) {
     
     var args = {
         data: {
@@ -26,4 +26,49 @@ exports.translate = function(message){
       });
     console.log();  
     });
+
+*/
+    var request = require('request');
+
+    var dataString = `{"text": "${msg}","from": "","to": "en"}`;
+
+    var header = {
+    'Content-Type': 'application/json'
+    }
+
+    var options = {
+        url: 'http://www.transltr.org/api/translate',
+        method: 'POST',
+        body: dataString,
+        headers: header
+    };
+
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body.translationText);
+            restClient.get(translateURL, function (data_tr, res) {
+    
+            var args = {
+                data: {
+                    "color":"green",
+                    "message":body.translationText,
+                    "notify":false,
+                    "message_format":"text"
+                },
+                headers: { "Content-Type": "application/json" }
+            };
+            restClient.post(URL, args, function (data, res) {
+              });
+                    console.log("ERROR");  
+            });
+        }else   
+        console.log(response);
+    }
+
+    request(options, callback);
+
+
+
+
+
 }
